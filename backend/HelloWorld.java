4556696f0ae5230c9726efe473c4cd2d3b271c1a
SomeUser123872 <myname213525@gmail.com>
2025-07-06 23:02:43 -0400

    Update HelloWorld.java

--- backend/HelloWorld.java
+++ backend/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
-    public sFtatic void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello from the backend");
     }
 }
